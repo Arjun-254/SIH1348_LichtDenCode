b24c1b75c57315b1c430e671f9a8699a69308186
Hetens <92987462+Hetens@users.noreply.github.com>
2023-09-06 22:35:34 +0530

    tts complete

--- src/components/Mic.js
+++ src/components/Mic.js
@@ -4,14 +4,29 @@ import { Bars, CirclesWithBar } from "react-loader-spinner";
 const Mic = () => {
   // State variables
   const [transcription, setTranscription] = useState("");
+
+  //mic permission
   const [permission, setPermission] = useState(false);
+
+  //recording inactive and paused are 3 states
   const [recordingStatus, setRecordingStatus] = useState("inactive");
+
+  //audio from the getUserMedia function
   const [stream, setStream] = useState(null);
+
+  //will contain encoded pieces of audio
   const [audioChunks, setAudioChunks] = useState([]);
+
+  //blob url of the audio
   const [audio, setAudio] = useState(null);
+
+  //blob url of reply
+  const[audio1,setAudio1] = useState(null);
+
   const [clicktranscribe, setclicktranscribe] = useState(false);
 
   const mimeType = "audio/webm";
+  //in built api reference
   const mediaRecorder = useRef(null);
 
   // Function to request microphone permission
@@ -35,8 +50,13 @@ const Mic = () => {
   // Function to start audio recording
   const startRecording = async () => {
     setRecordingStatus("recording");
+    //create new Media recorder instance using the stream
+
     const media = new MediaRecorder(stream, { type: mimeType });
+    //set the MediaRecorder instance to the mediaRecorder ref
+
     mediaRecorder.current = media;
+    //invokes the start method to start the recording process
 
     let localAudioChunks = [];
     mediaRecorder.current.ondataavailable = (event) => {
@@ -52,9 +72,13 @@ const Mic = () => {
   // Function to stop audio recording
   const stopRecording = () => {
     setRecordingStatus("inactive");
+    //stops the recording instance
+
     mediaRecorder.current.stop();
     mediaRecorder.current.onstop = () => {
+      //creates a blob file from the audiochunks data
       const audioBlob = new Blob(audioChunks, { type: mimeType });
+      //creates a playable URL from the blob file.
       const audioUrl = URL.createObjectURL(audioBlob);
       setAudio(audioUrl);
 
@@ -71,14 +95,17 @@ const Mic = () => {
       alert("Please select an audio file");
       return;
     }
-
+    // Create a Blob from the recorded audio
     const audioBlob = await fetch(audio).then((res) => res.blob());
+
+    // Create a FormData object and append the Blob as "file"
+
     const formData = new FormData();
     formData.append("file", audioBlob, "audio.webm");
 
     try {
       const response = await fetch(
-        "https://aabe-34-124-150-43.ngrok-free.app/transcribe/",
+        "https://3f61-34-124-150-43.ngrok-free.app/transcribe/",
         {
           method: "POST",
           body: formData,
@@ -97,6 +124,36 @@ const Mic = () => {
     }
     setclicktranscribe(false);
   };
+
+  const handleSound = async () => {
+    try {
+      const response = await fetch(
+        "https://3f61-34-124-150-43.ngrok-free.app/coqui-tts/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json", // Specify JSON content type
+          },
+          body: JSON.stringify({
+            text: transcription,
+            emotion: "cheerful",
+          }),
+        }
+      );
+  
+      if (response.ok) {
+        const responseData = await response.blob(); // Get the binary response data
+        const audioUrl = URL.createObjectURL(responseData); // Create a URL for the blob
+        setAudio1(audioUrl);
+      } else {
+        alert("Reply Failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("An error occurred while replying to the audio");
+    }
+  };
+  
 
   return (
     <div className="bg-gradient-to-r bg-cover bg-center from-blue-100 via-white to-cyan-300 min-h-screen p-10">
@@ -180,6 +237,11 @@ const Mic = () => {
             >
               Transcribe
             </button>
+            <button onClick={handleSound}
+            className ="bg-gradient-to-r from-pink-300 via-violet-300 to-purple-400 hover:bg-blue-700 text-white font-bold py-2 px-6 ml-2 rounded-full focus:outline-none focus:shadow-outline">
+                Reply
+            </button>
+            <audio src={audio1} controls className="mb-2"></audio>
           </div>
         </div>
       </div>
