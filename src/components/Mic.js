a9ac5762663b180c36d5d6a3d0190b95b873b06d
Arjun <arjun.shah254@gmail.com>
2023-09-06 19:59:49 +0530

    first commit

---
+++ src/components/Mic.js
@@ -0,0 +1,190 @@
+import React, { useState, useRef } from "react";
+import { Bars, CirclesWithBar } from "react-loader-spinner";
+
+const Mic = () => {
+  // State variables
+  const [transcription, setTranscription] = useState("");
+  const [permission, setPermission] = useState(false);
+  const [recordingStatus, setRecordingStatus] = useState("inactive");
+  const [stream, setStream] = useState(null);
+  const [audioChunks, setAudioChunks] = useState([]);
+  const [audio, setAudio] = useState(null);
+  const [clicktranscribe, setclicktranscribe] = useState(false);
+
+  const mimeType = "audio/webm";
+  const mediaRecorder = useRef(null);
+
+  // Function to request microphone permission
+  const getMicrophonePermission = async () => {
+    if ("MediaRecorder" in window) {
+      try {
+        const streamData = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: false,
+        });
+        setPermission(true);
+        setStream(streamData);
+      } catch (err) {
+        alert(err.message);
+      }
+    } else {
+      alert("The MediaRecorder API is not supported in your browser.");
+    }
+  };
+
+  // Function to start audio recording
+  const startRecording = async () => {
+    setRecordingStatus("recording");
+    const media = new MediaRecorder(stream, { type: mimeType });
+    mediaRecorder.current = media;
+
+    let localAudioChunks = [];
+    mediaRecorder.current.ondataavailable = (event) => {
+      if (typeof event.data === "undefined") return;
+      if (event.data.size === 0) return;
+      localAudioChunks.push(event.data);
+    };
+    setAudioChunks(localAudioChunks);
+
+    mediaRecorder.current.start();
+  };
+
+  // Function to stop audio recording
+  const stopRecording = () => {
+    setRecordingStatus("inactive");
+    mediaRecorder.current.stop();
+    mediaRecorder.current.onstop = () => {
+      const audioBlob = new Blob(audioChunks, { type: mimeType });
+      const audioUrl = URL.createObjectURL(audioBlob);
+      setAudio(audioUrl);
+
+      const formData = new FormData();
+      formData.append("file", audio);
+      setAudioChunks([]);
+    };
+  };
+
+  // Function to transcribe audio
+  const handleTranscribe = async () => {
+    setclicktranscribe(true);
+    if (!audio) {
+      alert("Please select an audio file");
+      return;
+    }
+
+    const audioBlob = await fetch(audio).then((res) => res.blob());
+    const formData = new FormData();
+    formData.append("file", audioBlob, "audio.webm");
+
+    try {
+      const response = await fetch(
+        "https://9031-34-173-122-139.ngrok-free.app/transcribe/",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setTranscription(data.text);
+      } else {
+        alert("Transcription failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("An error occurred while transcribing the audio");
+    }
+    setclicktranscribe(false);
+  };
+
+  return (
+    <div className="bg-gradient-to-r bg-cover bg-center from-blue-200 via-white to-cyan-300 min-h-screen">
+      <div className="fixed bottom-0 left-0 right-0 bg-gray-100 border-t border-gray-300">
+        <div className="flex flex-row justify-between w-full m-2 p-4 ">
+          <div className="audio-controls space-y-2 flex flex-row justify-center items-center">
+            {!permission ? (
+              <button
+                onClick={getMicrophonePermission}
+                type="button"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
+              >
+                Get Microphone
+              </button>
+            ) : null}
+            {permission && recordingStatus === "inactive" ? (
+              <button
+                onClick={startRecording}
+                type="button"
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
+              >
+                Start Recording
+              </button>
+            ) : null}
+            {recordingStatus === "recording" ? (
+              <div className="flex justify-center items-center flex-row my-4">
+                <button
+                  onClick={stopRecording}
+                  type="button"
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline mr-2"
+                >
+                  Stop Recording
+                </button>
+
+                <Bars
+                  height="40"
+                  width="40"
+                  color="#009CFF"
+                  ariaLabel="bars-loading"
+                  wrapperStyle={{}}
+                  wrapperClass=""
+                  visible={true}
+                />
+              </div>
+            ) : null}
+          </div>
+          {audio ? (
+            <div className="audio-container flex flex-row justify-center items-center">
+              <audio src={audio} controls className="mb-2"></audio>
+              <a
+                download
+                href={audio}
+                className="text-blue-500 hover:text-blue-700"
+              ></a>
+            </div>
+          ) : null}
+          <div className="flex justify-center items-center flex-row my-4">
+            {clicktranscribe && (
+              <CirclesWithBar
+                height="40"
+                width="40"
+                color="#009CFF"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                outerCircleColor=""
+                innerCircleColor=""
+                barColor=""
+                ariaLabel="circles-with-bar-loading"
+              />
+            )}
+            <button
+              onClick={handleTranscribe}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 ml-2 rounded-full focus:outline-none focus:shadow-outline"
+            >
+              Transcribe
+            </button>
+            {transcription && (
+              <div className="mt-4">
+                <h2 className="text-xl font-semibold">Transcription:</h2>
+                <p className="mt-2">{transcription}</p>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Mic;
